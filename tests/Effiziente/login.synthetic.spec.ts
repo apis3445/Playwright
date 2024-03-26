847bb57c131fc916aaa97002c2a0759574f510e6
abigailarmijohernandez <abi1521@hotmail.com>
2024-03-25 21:41:33 -0600

    feature: add login synthetic test

---
+++ tests/Effiziente/login.synthetic.spec.ts
@@ -0,0 +1,16 @@
+import { expect, test } from '@playwright/test';
+
+test.describe('Synthetic login testing', () => {
+
+    // eslint-disable-next-line playwright/expect-expect
+    test('Should show dashboard', async ({ page }) => {
+        await page.goto('https://effizientedemo.azurewebsites.net/login');
+        await page.getByLabel('Company').fill('Demo');
+        await page.getByLabel('User').fill('Demo');
+        await page.getByPlaceholder('Password').fill('Demo');
+        await page.getByRole('button', { name: 'Login' }).click();
+        await expect(page.locator('app-card-pie').getByRole('img')).toBeVisible();
+
+    });
+
+});
