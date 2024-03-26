de6d45a86773ca8c553bc10340eeab5bca3ea686
abigailarmijohernandez <abi1521@hotmail.com>
2024-03-25 21:42:17 -0600

    fix: Increase timeout

--- tests/Effiziente/login.synthetic.spec.ts
+++ tests/Effiziente/login.synthetic.spec.ts
@@ -9,7 +9,7 @@ test.describe('Synthetic login testing', () => {
         await page.getByLabel('User').fill('Demo');
         await page.getByPlaceholder('Password').fill('Demo');
         await page.getByRole('button', { name: 'Login' }).click();
-        await expect(page.locator('app-card-pie').getByRole('img')).toBeVisible();
+        await expect(page.locator('app-card-pie').getByRole('img')).toBeVisible({ timeout: 20_000 });
 
     });
 
