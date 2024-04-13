8335de755c32e5b0d2d436db772397b97f66a416
abigailarmijohernandez <abi1521@hotmail.com>
2024-04-12 22:05:40 -0600

    feature: Change to english and server test

--- tests/Effiziente/servers.spec.ts
+++ tests/Effiziente/servers.spec.ts
@@ -1,6 +1,6 @@
 import { test } from '@playwright/test';
 import { ServersPage } from '../../pages/Effiziente/serversPage';
-import { faker } from '@faker-js/faker';
+//import { faker } from '@faker-js/faker';
 
 test.describe('Servers', () => {
     test.use({ storageState: 'auth/admin.json' });
@@ -9,9 +9,9 @@ test.describe('Servers', () => {
         const serversPage = new ServersPage(page);
         await serversPage.goTo();
 
-        const key = faker.number.int({ min: 2, max: 100 });
-        const name = faker.company.name();
-        const url = faker.internet.url();
+        // const key = faker.number.int({ min: 2, max: 100 });
+        // const name = faker.company.name();
+        // const url = faker.internet.url();
         await serversPage.add.click();
     });
 });
