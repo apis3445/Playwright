d3686e61623bfefd7dd6cd45a49b0f6efe26af12
abigailarmijohernandez <abi1521@hotmail.com>
2024-06-29 09:20:42 -0600

    fix: Environment variable

--- tests/Synthetic/login.spec.ts
+++ tests/Synthetic/login.spec.ts
@@ -6,8 +6,8 @@ test.describe('Synthetic login testing', () => {
     test('Should show dashboard', async ({ page }) => {
         await page.goto('https://effizientedemo.azurewebsites.net');
         await page.getByLabel('Company').fill(process.env.EFFIZIENTE_COMPANY!);
-        await page.getByLabel('User').fill(process.env.NORMAL_USER!);
-        await page.getByPlaceholder('Password').fill(process.env.NORMAL_PASSWORD!);
+        await page.getByLabel('User').fill(process.env.EFFIZIENTE_NORMAL_USER!);
+        await page.getByPlaceholder('Password').fill(process.env.EFFIZIENTE_NORMAL_PASSWORD!);
         await page.getByRole('button', { name: 'Login' }).click();
         await page.locator('app-card-pie').getByRole('img').click({ timeout: 35_000 });
     });
